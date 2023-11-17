d00cca0320af458d5d34280d6f9519168fd15de3
cannox227 <tommaso.canova00@gmail.com>
2023-11-17 18:51:55 +0200

    chore: logstd update

--- algos/ppo_utils.py
+++ algos/ppo_utils.py
@@ -4,9 +4,12 @@ from torch.distributions import Normal, Independent, MultivariateNormal
 import numpy as np
 
 class Policy(torch.nn.Module):
-    def __init__(self, state_space, action_space, hidden_size=32):
+    def __init__(self, state_space, action_space, hidden_size=32, base_logstd=1):
         super().__init__()
-        
+        self.state_space = state_space
+        self.action_space = action_space
+        self.hidden_size = hidden_size
+
         self.critic = torch.nn.Sequential(
             torch.nn.Linear(state_space, hidden_size),
             torch.nn.ReLU(),
@@ -23,7 +26,8 @@ class Policy(torch.nn.Module):
             torch.nn.Linear(hidden_size, action_space)
         )
 
-        self.actor_logstd = torch.nn.Parameter(1, action_space)
+        self.base_logstd = base_logstd
+        self.actor_logstd = torch.ones(action_space) * base_logstd
 
         self.critic.apply(self.init_weights)
         self.actor_mean.apply(self.init_weights)
@@ -39,5 +43,7 @@ class Policy(torch.nn.Module):
         action_std = torch.exp(action_logstd)
         cov_matrix = torch.mul(torch.eye(self.action_space), action_std)
         action_dist = MultivariateNormal(action_mean, cov_matrix)
-
         return action_dist, self.critic(x)
+    
+    def set_logstd_ratio(self, ratio_of_episodes):
+        self.actor_logstd = torch.ones(1, self.action_space) * self.base_logstd * ratio_of_episodes
